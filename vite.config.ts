821d2449206160cc99e8779434f4a75c58b3114a
Husni Mubarok <husniiimubarok00@gmail.com>
2026-04-20 19:58:00 +0700

    bismillah99x

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
+    tailwindcss(), // ← tambahkan ini
   ],
   resolve: {
     alias: {
